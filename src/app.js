a8b3b1c5ac9864a0e047a2ef814b95ce55950f4d
Om-khairnar <om.khairnar175@gmail.com>
2025-01-07 20:15:50 +0530

    Solving some Error from the Deployment

--- src/app.js
+++ src/app.js
@@ -1,42 +1,10 @@
-// import express from "express";
-// import cors from "cors";
-// import cookieParser from "cookie-parser";
-
-// const app = express();
-
-// // here read the cors pakage its basically choose which origin specificalyy you can allow
-// app.use(
-//   cors({
-//     origin: process.env.CORS_ORIGIN,
-//     credentials: true,
-//   })
-// );
-
-// app.use(express.json({ limit: "16kb" }));
-// app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-// app.use(express.static("public")); // files folder for bulic asset of my public folder
-// app.use(cookieParser());
-
-// //Routes import
-// import userRouter from "./routes/user.routes.js";
-// import blogRouter from "./routes/blog.routes.js";
-// import commentRouter from "./routes/comment.routes.js"
-
-// //Routes declaration
-// //http://localhost:8000/api/v1/users/register
-// app.use("/api/v1/users", userRouter);
-// app.use("/api/v1/blogs", blogRouter);
-// app.use("/api/v1/comment", commentRouter);
-// export { app };
-
-
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
 
-// CORS Configuration
+// here read the cors pakage its basically choose which origin specificalyy you can allow
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
@@ -46,17 +14,49 @@ app.use(
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public"));
+app.use(express.static("public")); // files folder for bulic asset of my public folder
 app.use(cookieParser());
 
-// Routes import
+//Routes import
 import userRouter from "./routes/user.routes.js";
 import blogRouter from "./routes/blog.routes.js";
-import commentRouter from "./routes/comment.routes.js";
+import commentRouter from "./routes/comment.routes.js"
 
-// Routes declaration
+//Routes declaration
+//http://localhost:8000/api/v1/users/register
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/blogs", blogRouter);
 app.use("/api/v1/comment", commentRouter);
-
 export { app };
+
+
+// import express from "express";
+// import cors from "cors";
+// import cookieParser from "cookie-parser";
+
+// const app = express();
+
+// // CORS Configuration
+// app.use(
+//   cors({
+//     origin: process.env.CORS_ORIGIN,
+//     credentials: true,
+//   })
+// );
+
+// app.use(express.json({ limit: "16kb" }));
+// app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+// app.use(express.static("public"));
+// app.use(cookieParser());
+
+// // Routes import
+// import userRouter from "./routes/user.routes.js";
+// import blogRouter from "./routes/blog.routes.js";
+// import commentRouter from "./routes/comment.routes.js";
+
+// // Routes declaration
+// app.use("/api/v1/users", userRouter);
+// app.use("/api/v1/blogs", blogRouter);
+// app.use("/api/v1/comment", commentRouter);
+
+// export { app };
--- src/index.js
+++ src/index.js
@@ -1,36 +1,37 @@
-// //require('dotenv').config()
 // import dotenv from "dotenv";
 // import connectDB from "./db/db.js";
 // import { app } from "./app.js";
+
 // dotenv.config({
 //   path: "./.env",
 // });
 
 // connectDB()
 //   .then(() => {
-//     app.listen(process.env.PORT || 8000, () => {
-//       console.log(`Server is running at port : ${process.env.PORT}`);
-//     });
+//     console.log(`MongoDB connected successfully`);
 //   })
 //   .catch((err) => {
-//     console.log("MONGO db connection failed !!!");
+//     console.log("MONGO db connection failed!!!");
 //   });
 
+// // Export the app as the handler for Vercel
+// export default app;
+
+
+//require('dotenv').config()
 import dotenv from "dotenv";
 import connectDB from "./db/db.js";
 import { app } from "./app.js";
-
 dotenv.config({
   path: "./.env",
 });
 
 connectDB()
   .then(() => {
-    console.log(`MongoDB connected successfully`);
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
   })
   .catch((err) => {
-    console.log("MONGO db connection failed!!!");
+    console.log("MONGO db connection failed !!!");
   });
-
-// Export the app as the handler for Vercel
-export default app;
